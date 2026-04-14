ddb6b7b992392bba57a07088444aee4d87e136ca
sayan30105 <maitysayandeep313@gmail.com>
2026-04-14 23:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
+import streamlit as st
+from predict import predict_url
 
-app = Flask(__name__)
+st.title("🔐 URL Phishing Detector")
 
-# Load model
-model = pickle.load(open('../model.pkl', 'rb'))
+url = st.text_input("Enter URL")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    features = [float(x) for x in request.form.values()]
-    final_features = np.array([features])
-
-    prediction = model.predict(final_features)
-
-    result = "Safe URL ✅" if prediction[0] == 0 else "Phishing URL 🚨"
-
-    return render_template('index.html', prediction_text=result)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if st.button("Check"):
+    if url:
+        result = predict_url(url)
+        
+        if result == "Phishing":
+            st.error("⚠️ Phishing URL")
+        else:
+            st.success("✅ Safe URL")
+    else:
+        st.warning("Enter a URL")
